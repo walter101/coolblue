4d83b0d66f6629795c3af1663187ee21fc1b411b
walter pothof <walterpothof@gmail.com>
2016-08-21 11:50:48 +0200

    basis coolblue met menu links en hover effect rechts template

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -17,5 +18,14 @@ class DefaultController extends Controller
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
         ]);
+    }
+    
+    
+    
+    public function testAction($myvar){
+        return new Response(
+            '<html><body>Dit is een stukje tekst ( '.$myvar.' )</body></html>'
+        );
+
     }
 }
